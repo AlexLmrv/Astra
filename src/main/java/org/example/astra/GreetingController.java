151dedb56c422ae008cba678553847618fb69825
AlexLmrv <AlexLmrv@users.noreply.github.com>
2020-01-31 23:29:47 +0700

    Добавлены сартовые страницы, PostgreSQL, поиск сообщений

--- src/main/java/org/example/astra/GreetingController.java
+++ src/main/java/org/example/astra/GreetingController.java
@@ -1,14 +1,20 @@
 package org.example.astra;
 
+import org.example.astra.domain.Message;
+import org.example.astra.repos.MessageRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
 import java.util.Map;
 
 @Controller
 public class GreetingController {
+    @Autowired MessageRepo messageRepo;
 
     @GetMapping("/greeting")
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Map<String, Object> model) {
@@ -18,8 +24,38 @@ public class GreetingController {
 
     @GetMapping
     public String main(Map<String, Object> model){
-        model.put("hello", "Hello, web!");
+        Iterable<Message> messages = messageRepo.findAll();
+
+        model.put("messages", messages);
         return "main";
     }
+
+    @PostMapping
+    public String add(@RequestParam String text, @RequestParam String tag, Map<String, Object> model){
+        Message message = new Message(text, tag);
+        messageRepo.save(message);
+
+        Iterable<Message> messages = messageRepo.findAll();
+        model.put("messages", messages);
+
+        return "main";
+    }
+
+    @PostMapping("filter")
+    public String filter(@RequestParam String filter, Map<String, Object> model){
+        Iterable<Message> messages;
+
+        if (filter != null && !filter.isEmpty() ) {
+            messages = messageRepo.findByTag(filter);
+        }
+        else {
+            messages = messageRepo.findAll();
+        }
+
+        model.put("messages", messages);
+
+        return "main";
+    }
+
 
 }
--- src/main/java/org/example/astra/repos/MessageRepo.java
+++ src/main/java/org/example/astra/repos/MessageRepo.java
@@ -3,5 +3,11 @@ package org.example.astra.repos;
 import org.springframework.data.repository.CrudRepository;
 import org.example.astra.domain.Message;
 
+import java.util.List;
+
 public interface MessageRepo extends CrudRepository<Message, Integer> {
+
+    List<Message> findByTag(String text);
+
+
 }
