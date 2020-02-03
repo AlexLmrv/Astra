c4b3535d0a8a4a8a8265a3f2ffc543af03c54d8f
AlexLmrv <AlexLmrv@users.noreply.github.com>
2020-02-04 00:30:59 +0700

    Установлен freemarker
    - переделаны шаблоны
    - исправлена ошибка с отображением списка пользователей
    - остался баг с записью сообщений

--- src/main/java/org/example/astra/controller/ProfileController.java
+++ src/main/java/org/example/astra/controller/ProfileController.java
@@ -7,6 +7,7 @@ import org.example.astra.repos.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,10 +20,10 @@ public class ProfileController {
     //@Autowired UserRepo userRepo;
 
     @GetMapping("/profile")
-    public String add(
+    public String profile(
             @AuthenticationPrincipal User user,
-            Map<String, Object> model){
-        model.put("user", user);
+            Model model){
+        model.addAttribute("user", user);
 
         return "profile";
     }
