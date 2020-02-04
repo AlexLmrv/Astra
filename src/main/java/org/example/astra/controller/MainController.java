6ee067171ec3600463dfb0afb304441f03685024
AlexLmrv <AlexLmrv@users.noreply.github.com>
2020-02-04 22:32:46 +0700

    Добавлена загрузка файлов + раздача статики

--- src/main/java/org/example/astra/controller/MainController.java
+++ src/main/java/org/example/astra/controller/MainController.java
@@ -5,20 +5,28 @@ import org.example.astra.domain.User;
 import org.example.astra.repos.MessageRepo;
 import org.example.astra.repos.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Map;
+import java.util.UUID;
 
 @Controller
 public class MainController {
     @Autowired MessageRepo messageRepo;
     @Autowired UserRepo userRepo;
+
+    @Value("${upload.path}")   // берёт значения из properties
+    private String uploadPath;
 
     @GetMapping("/")
     public String greeting(Map<String, Object> model) {
@@ -50,8 +58,24 @@ public class MainController {
             @RequestParam String text,
             @RequestParam String tag,
             @RequestParam(required = false, defaultValue = "") String filter,
-            Model model){
+            @RequestParam("file") MultipartFile file,
+            Model model) throws IOException {
         Message message = new Message(text, tag, user);
+
+        if(file != null && !file.getOriginalFilename().isEmpty()){
+            File uploadDir = new File(uploadPath);
+
+            if (!uploadDir.exists()){   //проверяем, существует ли uploadPath, если нет, то создаём
+                uploadDir.mkdir();
+            }
+
+            String uuidFile = UUID.randomUUID().toString(); //генерим universe unique id
+            String resultFileName = uuidFile + "." + file.getOriginalFilename(); //конкатенируем с исходным названием
+
+            file.transferTo(new File(uploadPath + "/" + resultFileName));
+            message.setFilename(resultFileName); //и вот конечное имя файла
+        }
+
         messageRepo.save(message);
 
         Iterable<User> users = userRepo.findAll();
